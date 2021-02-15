e7a027d8fb4d2ffbaf04299abcf7899e51effeb8
Christian Pari <christianpari@outlook.com>
2021-02-14 20:44:56 -0500

    TBH not sure why these all these are appearing as new files...

--- CLI-Version/src/com/christianpari/Main.java
+++ CLI-Version/src/com/christianpari/Main.java
@@ -3,6 +3,18 @@ package com.christianpari;
 public class Main {
 
   public static void main(String[] args) {
+    Application app = new Application();
 
+    app.createUser();
+    User user1 = app.getUser(1);
+    user1.addContact();
+    System.out.println(user1.getDirectory().displayDirectory());
+
+    app.createUser();
+    User user2 = app.getUser(2);
+    user2.addContact();
+    System.out.println(user2.getDirectory().displayDirectory());
+
+    System.out.println(app.displayUsers());
   }
 }
--- README.md
+++
@@ -1,3 +0,0 @@
-# Company HR Directory Application
-I will be creating an Application that will allow a company's HR to have a full directory of information, whether it be another 
-company's info or an employees'.
