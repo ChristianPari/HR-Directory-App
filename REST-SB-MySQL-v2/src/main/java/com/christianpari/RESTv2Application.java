ef4bd90b027f4be94f5616cd56d62fd49dc0f8ee
Christian Pari <christianpari@outlook.com>
2021-02-19 19:00:27 -0500

    Began creation of version 2

---
+++ REST-SB-MySQL-v2/src/main/java/com/christianpari/RESTv2Application.java
@@ -0,0 +1,13 @@
+package com.christianpari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTv2Application.class, args);
+	}
+
+}
