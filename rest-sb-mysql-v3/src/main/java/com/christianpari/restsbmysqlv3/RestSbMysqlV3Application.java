35c6984a378aee9047c2ab6538b864f3e27efd24
Christian Pari <christianpari@outlook.com>
2021-03-03 19:55:47 -0500

    Created a version 3, looking into a different React FE, wasnt satisified with how the last was looking, also need more work w/ React to refresh memory so restarting

---
+++ rest-sb-mysql-v3/src/main/java/com/christianpari/restsbmysqlv3/RestSbMysqlV3Application.java
@@ -0,0 +1,13 @@
+package com.christianpari.restsbmysqlv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSbMysqlV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSbMysqlV3Application.class, args);
+	}
+
+}
---
+++ rest-sb-mysql-v3/src/test/java/com/christianpari/restsbmysqlv3/RestSbMysqlV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.christianpari.restsbmysqlv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSbMysqlV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
