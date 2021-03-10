a94c947d70d6e469f0ded8738c8f720c47b2cafe
Christian Pari <christianpari@outlook.com>
2021-03-09 19:59:53 -0500

    working on creating a security package using SpringSecurity

--- rest-sb-mysql-v3/src/main/java/com/christianpari/restsbmysqlv3/repository/UserRepository.java
+++ rest-sb-mysql-v3/src/main/java/com/christianpari/restsbmysqlv3/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, String> {
   User findUserByEmail(String email);
   User findUserById(String id);
+  Boolean existsByEmail(String email);
 }
